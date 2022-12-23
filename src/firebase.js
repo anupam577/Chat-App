af277b462465081ace82571b31f27ad96084e831
Anupam singh <91001107+anupam577@users.noreply.github.com>
2022-12-24 04:00:10 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+//   authDomain: "chat-ab746.firebaseapp.com",
+//   projectId: "chat-ab746",
+//   storageBucket: "chat-ab746.appspot.com",
+//   messagingSenderId: "901216368405",
+//   appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyDgdLUyTqTsO6iJUc8RmuGIZwnIuiBs_JI",
+  authDomain: "chatapp-fa721.firebaseapp.com",
+  projectId: "chatapp-fa721",
+  storageBucket: "chatapp-fa721.appspot.com",
+  messagingSenderId: "8904959890",
+  appId: "1:8904959890:web:cd0089229f12b021a6034b"
 };
 
 // Initialize Firebase
